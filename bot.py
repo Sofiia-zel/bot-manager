b99c2dc08b6e42fb30a32bffed38b370267b04f9
sofiia <186623782+ofiia-zel@users.noreply.github.com>
2024-11-10 16:32:31 +0200

    added 'show_people' and 'show_events' funcs; 'add_new_event' was improved

--- bot.py
+++ bot.py
@@ -1,18 +1,14 @@
-import requests
 import telebot
 from telebot import types, TeleBot
 from dotenv import load_dotenv
 import os
-from notion_integration import create_page
-
-
-# from app.add_new_event.new_event import process_name_step, process_date_step, process_greeting_text_step
-
+from datetime import datetime, timezone, timedelta, tzinfo
+from notion_integration import create_page, show_people_names, show_existing_events
+import pytz
 
 
 load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-
 bot = TeleBot(TELEGRAM_TOKEN)
 
 # Хранение данных о новом событии
@@ -26,7 +22,6 @@ def start(message):
     update_btn = types.KeyboardButton("🔄 Оновити подію")
     delete_btn = types.KeyboardButton("❌ Видалити подію")
     show_people_btn = types.KeyboardButton("👥 Показати імена та назви свят")
-
     keyboard.add(add_event_btn, show_events_btn, update_btn, delete_btn, show_people_btn)
     bot.send_message(message.chat.id, "Вас вітає бот-менеджер! Виберіть необхідну функцію з переліку.", reply_markup=keyboard)
 
@@ -34,104 +29,94 @@ def start(message):
 # ОБРОБКА СТВОРЕННЯ НОВОЇ ПОДІЇ, перехоплення потрібної інформації від користувача
 @bot.message_handler(func=lambda message: message.text == "➕ Додати нову подію")
 def add_new_event(message):
-    msg = bot.send_message(message.chat.id, "Введіть ім'я або назву події:")
-    bot.register_next_step_handler(msg, process_name_step)
+    msg = bot.send_message(message.chat.id, "Введіть ім'я/назву події або введіть 'відміна':")
 
+    def name_or_cancel_step(name_message):
+        if name_message.text.lower() == "відміна":
+            # Если введено "відміна", показываем главное меню и подтверждаем отмену
+            bot.send_message(message.chat.id, "Відміну підтверджено!")
+            start(name_message)
+        else:
+            # Передаём название в process_name_step для обработки
+            process_name_step(name_message)
+    bot.register_next_step_handler(msg, name_or_cancel_step)
 
 def process_name_step(message):
     # Сохраняем введённое имя/название в структуру user_data
     user_data["Ім'я/назва"] = {"title": [{"text": {"content": message.text}}]}
-    msg = bot.send_message(message.chat.id, "Введіть дату у форматі YYYY-MM-DD:")
+    # Переходим к следующему шагу — ввод даты
+    msg = bot.send_message(message.chat.id, "Введіть дату у форматі DD-MM-YYYY:")
     bot.register_next_step_handler(msg, process_date_step)
 
 
 def process_date_step(message):
-    # Сохраняем введённую дату в структуру user_data
-    user_data["Дата"] = {"date": {"start": message.text, "end": None}}
-    msg = bot.send_message(message.chat.id, "Введіть текст привітання:")
-    bot.register_next_step_handler(msg, process_greeting_text_step)
+    # Форматируем дату и добавляем время 08:00
+    """
+    Notion глючит из-за часового пояса. Время принимается в GMT 0,
+    а по Киеву GMT+2. Выходит, что время ставиться на 2 часа раньше.
+    Пускай программа работает только по Киеву.
+    """
+    try:
+        date = datetime.strptime(message.text, '%d-%m-%Y')
+        # date_needed = date.replace(hour=8, minute=0).astimezone(timezone.utc).isoformat()
+        date_needed = get_kiev_time(date)
+        user_data["Дата"] = {"date": {"start": date_needed, "end": None, "time_zone": "Europe/Kiev"}}
+        msg = bot.send_message(message.chat.id, "Введіть текст привітання:")
+        bot.register_next_step_handler(msg, process_greeting_text_step)
+    except ValueError:
+        msg = bot.send_message(message.chat.id, "Некоректний формат дати! Спробуйте ще раз:")
+        bot.register_next_step_handler(msg, process_date_step)
 
 
 def process_greeting_text_step(message):
     # Сохраняем введённый текст привітання в структуру user_data
     user_data["Текст привітання"] = {"rich_text": [{"text": {"content": message.text}}]}
-    # Вызываем функцию для добавления записи в Notion
+    # Передаём все собранные данные в функцию для создания страницы
+    create_page(user_data)
+
     bot.send_message(message.chat.id, "Подію створено!")
 
-
-    # TODO Сделать логику для сохранения фото в папку, а потом выгрузки его в базу.
-    # elif step == "photo":
-    #     if message.content_type == "photo":
-    #         # Сохранение фото и завершение ввода данных
-    #         user_data[user_id]["photo"] = message.photo[-1].file_id  # Использование самого высокого качества фото
-    #         bot.send_message(message.chat.id, "Подія успішно збережена!")
-    #
-    #         # Сохранение данных в БД или другой системе хранения
-    #         # save_event_to_db(user_data[user_id])  # функция для сохранения
-    #
-    #         # Очистка данных пользователя из user_data
-    #         # del user_data[user_id]
-    #     else:
-    #         bot.send_message(message.chat.id, "Будь ласка, надішліть фото, а не текст.")
+# TODO Исправить ошибку с временем (или забить на нее)
+# Установка на 8:00 по Киеву для process_date_step
+def get_kiev_time(date):
+    kiev_tz = pytz.timezone('Europe/Kiev')
+    date = kiev_tz.localize(date.replace(hour=10, minute=0))  # устанавливаем 8:00 по киевскому времени
+    return date.astimezone(pytz.utc).isoformat()  # переводим в UTC для хранения в Notion
 
 
-    print(user_data)
+# TODO Сделать логику для сохранения фото в папку, а потом выгрузки его в базу.
 
 
+# ОБРОБКА ФУНКЦІЇ ПОКАЗАТИ ІМЕНА ПОДІЙ
+@bot.message_handler(func=lambda message: message.text == "👥 Показати імена та назви свят")
+def show_people(message):
+    msg = bot.send_message(message.chat.id, f"Імена іменинників та назви свят, наявні в базі:\n{show_people_names()}")
+    bot.send_message(message.chat.id, "Бажаєте побачити весь текст подій, додати нову подію або оновити наявну?\n"
+                                      "Ви можете зробити це за кнопками '📅 Показати події', '➕ Додати нову подію', "
+                                      "'🔄 Оновити подію'.")
 
 
-# @bot.message_handler(commands=['add_new_event'])
-# def start_add_event(message):
-#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#     markup.add(types.KeyboardButton("Отмена"))
-#     bot.send_message(
-#         message.chat.id,
-#         "Введите название события или нажмите 'Отмена' для выхода:",
-#         reply_markup=markup
-#     )
-#     bot.register_next_step_handler(message, get_title)
-#
-#
-# def get_title(message):
-#     if message.text.lower() == "отмена":
-#         bot.send_message(message.chat.id, "Добавление события отменено.", reply_markup=types.ReplyKeyboardRemove())
-#         return
-#
-#     user_data['title'] = message.text
-#     bot.send_message(message.chat.id, "Введите дату события (в формате ГГГГ-ММ-ДД):")
-#     bot.register_next_step_handler(message, get_date)
-#
-#
-# def get_date(message):
-#     if message.text.lower() == "отмена":
-#         bot.send_message(message.chat.id, "Добавление события отменено.", reply_markup=types.ReplyKeyboardRemove())
-#         return
-#
-#     user_data['date'] = message.text
-#     bot.send_message(message.chat.id, "Введите описание события:")
-#     bot.register_next_step_handler(message, get_description)
-#
-#
-# def get_description(message):
-#     if message.text.lower() == "отмена":
-#         bot.send_message(message.chat.id, "Добавление события отменено.", reply_markup=types.ReplyKeyboardRemove())
-#         return
-#
-#     user_data['description'] = message.text
-#
-#     # Отправка данных в Notion
-#     title = user_data.get('title')
-#     date = user_data.get('date')
-#     description = user_data.get('description')
-#
-#     success = add_event_to_notion(title, date, description)
-#
-#     if success:
-#         bot.send_message(message.chat.id, "Событие успешно добавлено в базу данных Notion!",
-#                          reply_markup=types.ReplyKeyboardRemove())
-#     else:
-#         bot.send_message(message.chat.id, "Произошла ошибка при добавлении события.",
-#                          reply_markup=types.ReplyKeyboardRemove())
+# ОБРОБКА ФУНКЦІЇ ПОКАЗАТИ ПОДІЇ
+@bot.message_handler(func=lambda message: message.text == "📅 Показати події")
+def show_events(message):
+    events = show_existing_events()
+    if isinstance(events, str):  # Если вернулась ошибка
+        bot.send_message(message.chat.id, events)
+        return
+
+    # Форматируем и выводим информацию о каждом событии
+    output = []
+    for details in events.values():
+        event_info = (
+            f"Ім'я/назва: {details['Ім\'я/назва']}\n"
+            f"Дата: {details['Дата']}\n"
+            f"Текст привітання: {details['Текст привітання']}\n"
+        )
+        output.append(event_info)
+
+    # Объединяем всё в один текст и выводим
+    final_output = "\n".join(output)
+    bot.send_message(message.chat.id, f"Імена іменинників та назви свят, наявні в базі:\n\n{final_output}")
 
 
 bot.polling()
--- notion_integration.py
+++ notion_integration.py
@@ -1,71 +1,118 @@
 from dotenv import load_dotenv
-import os
 import requests
 import threading    # для відстежування часу, тригер на час
-from datetime import datetime, timezone
-import json
+from datetime import datetime, timezone, timedelta
 import os
-from bot import user_data
 
-
-# load_dotenv()
-#
-# NOTION_DATABASE_TOKEN = os.getenv("NOTION_DATABASE_TOKEN")
-# DATABASE_ID = os.getenv("DATABASE_ID")
-# # TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-#
-# # Початок роботи з Notion
-# headers = {
-#     'Authorization': 'Bearer ' + NOTION_DATABASE_TOKEN,
-#     'Notion-Version': '2022-06-28',
-#     'Content-Type': 'application/json',
-# }
 
 # "object": "page" - это название одной строки таблицы
 
-# data: dict
-def create_page():
+# Функція для того, щоб не повторювати токени та хедерси у кожній наступній функції
+def get_notion_headers():
     load_dotenv()
-
     NOTION_DATABASE_TOKEN = os.getenv("NOTION_DATABASE_TOKEN")
     DATABASE_ID = os.getenv("DATABASE_ID")
-    # TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-
-    # Початок роботи з Notion
     headers = {
         'Authorization': 'Bearer ' + NOTION_DATABASE_TOKEN,
         'Notion-Version': '2022-06-28',
         'Content-Type': 'application/json',
     }
+    return headers, DATABASE_ID
 
-    name = "Test name"
-    greeting = "Test text"
-    date_needed = datetime.now().astimezone(timezone.utc).isoformat()
 
-    data = {
-        "Ім'я/назва": {"title": [{"text": {"content": name}}]},
-        "Текст привітання": {"rich_text": [{"text": {"content": greeting}}]},
-        "Дата": {"date": {"start": date_needed, "end": None}}
-    }
-
+def create_page(data: dict):
+    headers, DATABASE_ID = get_notion_headers()
     url = f'https://api.notion.com/v1/pages'
     payload = {'parent': {'database_id': DATABASE_ID}, 'properties': data}
-
     res = requests.post(url, headers=headers, json=payload)
-    print(res.status_code)
-
-
-    # try:
-    #     create_page()
-    # except Exception as e:
-    #     print(f"Помилка: {e}")
-
+    if res.status_code == 200:
+        print("Страница успешно создана в Notion.")
+    else:
+        print(f"Ошибка {res.status_code}: {res.text}")
     return res
 
 
-create_page()
+def show_people_names():
+    headers, DATABASE_ID = get_notion_headers()
+    url = f'https://api.notion.com/v1/databases/{DATABASE_ID}/query'
+    payload = {'page_size': 100}
+    response = requests.post(url, json=payload, headers=headers)
+    data = response.json()
+
+    if response.status_code != 200:
+        return f"Ошибка при запросе к API: {response.status_code}"
+
+    if 'results' not in data:
+        return "Ключ 'results' не найден в файле."
+
+    names = []  # Список для сбора всех имен и названий событий
+
+    pages = data['results']
+    for page in pages:
+        props = page['properties']
+        if "Ім'я/назва" in props:
+            if len(props["Ім'я/назва"]["title"]) > 0:
+                name = props["Ім'я/назва"]["title"][0]["text"]["content"]
+                names.append(name)  # Добавляем имя или название события в список
+            else:
+                names.append("Назва пуста.")
+        else:
+            names.append("Свойство 'Ім'я/назва' не найдено или пустое.")
+    # Объединяем все имена в одну строку с новой строкой между каждым именем
+    return "\n".join(names)
 
 
+def show_existing_events():
+    headers, DATABASE_ID = get_notion_headers()
+    url = f'https://api.notion.com/v1/databases/{DATABASE_ID}/query'
+    payload = {'page_size': 100}
+    response = requests.post(url, json=payload, headers=headers)
+    data = response.json()
+
+    if response.status_code != 200:
+        return f"Ошибка при запросе к API: {response.status_code}"
+
+    if 'results' not in data:
+        return "Ключ 'results' не найден в файле."
+
+    events_dict = {}  # Словарь для сбора информации о событиях
+    event_number = 1  # Начинаем с 1
+
+    pages = data['results']
+    for page in pages:
+        props = page['properties']
+
+        # Инициализируем структуру для одного события
+        event_data = {}
+
+        # Извлекаем значение "Ім'я/назва"
+        if "Ім'я/назва" in props and len(props["Ім'я/назва"]["title"]) > 0:
+            event_data["Ім'я/назва"] = props["Ім'я/назва"]["title"][0]["text"]["content"]
+        else:
+            event_data["Ім'я/назва"] = "Назва пуста."
+
+        # Извлекаем значение "Дата"
+        if "Дата" in props and "date" in props["Дата"] and props["Дата"]["date"] is not None:
+            full_date = props["Дата"]["date"]["start"]
+            # Обрезаем строку до символа 'T', используем форматирование
+            event_data["Дата"] = datetime.fromisoformat(full_date).strftime('%d-%m-%Y')
+        else:
+            event_data["Дата"] = "Дата не указана."
+
+        # Извлекаем значение "Текст привітання"
+        if "Текст привітання" in props and len(props["Текст привітання"]["rich_text"]) > 0:
+            event_data["Текст привітання"] = props["Текст привітання"]["rich_text"][0]["text"]["content"]
+        else:
+            event_data["Текст привітання"] = "Привітання відсутнє."
+
+        events_dict[event_number] = event_data
+        event_number += 1  # Увеличиваем номер для следующего события
+
+    return events_dict
+
+
+
+# тренировка
 # def get_pages():
 #     url = f'https://api.notion.com/v1/databases/{DATABASE_ID}/query'
 #     payload = {'page_size': 100}
@@ -76,13 +123,14 @@ create_page()
 #
 #     if response.status_code != 200:
 #         print(f"Ошибка: {response.status_code}")
-#         print(response.json())  # Выводим ответ для отладки
+#         # print(response.json())  # Выводим ответ для отладки
 #         return None
 #
 #     results = data['results']
 #     return results
 #
-# # pages = get_pages()  # Запит на діставання json
+#
+# pages = get_pages()  # Запит на діставання json
 #
 # with open('data.json', 'r', encoding='utf-8') as f:
 #     data = json.load(f)
